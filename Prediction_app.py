8548df4d649e34811e8bba48a87e475b2f45889a
Neeraj Vishwakarma <princevishwakarma7879@gmail.com>
2025-12-07 23:58:58 +0530

    Add files via upload

---
+++ Prediction_app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import streamlit as st
+import joblib as jb
+
+model = jb.load('model.pkl')
+
+st.header("Banglore House Price Predictor")
+
+data = pd.read_csv(r'C:\college projects\new minor complete project\Bengaluru_House_Data.csv')
+
+loc = st.selectbox("Choose the location ", data['location'].unique())
+sqft = st.number_input("Enter Total square feet ")
+beds = st.number_input("Enter No of bedrooms ")
+bath = st.number_input("Enter No of bathrooms ")
+balc = st.number_input("Enter No of balcony's ")
+
+Input = pd.DataFrame([[loc, sqft, bath, balc, beds]],
+                     columns=['location', 'total_sqft', 'bath', 'balcony', 'bedroom'])
+
+if st.button("Predict the price"):
+    output = model.predict(Input)
+    price = output[0] * 100000
+    st.write(f"🏠 The price of the house is: ₹ {price:,.2f}")
